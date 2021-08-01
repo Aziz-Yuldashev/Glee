0d6487ca0e170b2b73de8f12b3270a4f5cab0b94
Aziz-Yuldashev <riko.shepard@mail.ru>
2021-08-02 00:57:23 +0600

    added product page

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,17 @@
 $(function(){
+  $('.filter-price__input').ionRangeSlider({
+    type: "double",
+    prefix: "$",
+    step: 0.1,
+    onStart: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+    onChange: function (data){
+      $('.filter-price__from').text(data.from);
+      $('.filter-price__to').text(data.to);
+    },
+  });
 
   $ ('.top-slider__inner').slick({
     dots: true,
@@ -6,8 +19,17 @@ $(function(){
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000
+  
+  });
 
-  })
+  $(".filter-recent__star").rateYo({
+    starWidth: "11px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "7px",
+
+  });
   
   var Mixer = mixitup ( '.products__content' );
   var Mixer = mixitup ( '.design__content' );
