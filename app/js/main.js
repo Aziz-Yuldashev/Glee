5ab1427681d7b9eacf0eb6e17abf9ea0be201e90
Aziz-Yuldashev <riko.shepard@mail.ru>
2021-08-06 20:41:49 +0600

    added detalis

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,43 @@
-$(function(){
+$(function () {
+  $('.detalis-item__thumb').slick({
+    asNavFor: '.detalis-item__big',
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false,
+    
+
+  });
+
+  $('.detalis-item__big').slick({
+    asNavFor: '.detalis-item__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true,
+  });
+
   $('.filter-price__input').ionRangeSlider({
     type: "double",
     prefix: "$",
     step: 0.1,
-    onStart: function (data){
+    onStart: function (data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
     },
-    onChange: function (data){
+    onChange: function (data) {
       $('.filter-price__from').text(data.from);
       $('.filter-price__to').text(data.to);
     },
   });
 
-  $ ('.top-slider__inner').slick({
+  $('.top-slider__inner').slick({
     dots: true,
     arrows: false,
     fade: true,
     autoplay: true,
     autoplaySpeed: 2000
-  
+
   });
 
   $(".star").rateYo({
@@ -39,8 +57,23 @@ $(function(){
     spacing: "7px",
 
   });
+
+  $(".detalis-item__star").rateYo({
+    starWidth: "18px",
+    normalFill: "#d6d6d6",
+    ratedFill: "#ffcc00",
+    readOnly: true,
+    spacing: "7px",
+
+  });
+
+  $('.detalis-item__num').styler({
+
+  }); 
+
   
-  var Mixer = mixitup ( '.products__content' );
-  var Mixer = mixitup ( '.design__content' );
-  
+
+  var Mixer = mixitup('.products__content');
+  var Mixer = mixitup('.design__content');
+
 });
